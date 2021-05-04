091ae0c45936f23304634e8d427fe1c6fa9176f7
dbianco <dbianco@developmentgateway.org>
2021-05-03 23:45:08 -0300

    SEEDSDT-5 Setup staging environment

--- mongo/entrypoint.sh
+++ mongo/entrypoint.sh
@@ -9,6 +9,7 @@ cp $DOCKER_SCRIPTS_DIR/init_* /docker-entrypoint-initdb.d/
 bash $DOCKER_SCRIPTS_DIR/upsert_users.sh
 
 echo "Launching official entrypoint..."
-# `exec` here is important to pass signals to the database server process;
-# without `exec`, the server will be terminated abruptly with SIGKILL (see #276)
-exec bash /entrypoint.sh mongod
+
+
+exec mongod --fork --logpath /srv/logs/mongo.log
+exec tail -f /srv/logs/mongo.log
