5c7d15308acecfa43c5e52b1efe40c9484eed602
dbianco <dbianco@developmentgateway.org>
2021-05-17 10:44:20 -0300

    SEEDSDT-18 [FE] B5.2 Market concentration as measured by Herfindahl-Hirschman Index (HHI)SEEDSDT-20 Display regional data in the map

---
+++ ui/src/charts/MapChart.jsx
@@ -0,0 +1,63 @@
+import React, {useState} from 'react'
+import {ResponsiveChoropleth} from '@nivo/geo'
+import {injectIntl} from 'react-intl';
+import countries from "./world_countries.json";
+
+
+import './chart.scss'
+
+const Chart = ({height, options, intl}) => {
+
+
+
+    return (
+        <div style={{height:height}}>
+            {options && options.data && <ResponsiveChoropleth
+        data={options.data}
+        features={countries.features}
+        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+        colors={['#C4E765', '#96C11F', '#F9D133', '#FB9755', '#FB5555']}
+
+        width="600"
+        heigth="1300"
+        domain={[ 1 , 5000 ]}
+        unknownColor="#D1D2D4"
+        valueFormat=".2s"
+        projectionScale={400}
+        projectionTranslation={[ 0.35, 0.5 ]}
+        projectionRotation={[ 0, 0, 0 ]}
+        enableGraticule={false}
+        borderWidth={0.5}
+        borderColor="#fff"
+        isInteractive={true}
+        legends={[
+            {
+                anchor: 'bottom-left',
+                direction: 'column',
+                justify: true,
+                translateX: 20,
+                translateY: -100,
+                itemsSpacing: 0,
+                itemWidth: 94,
+                itemHeight: 18,
+                itemDirection: 'left-to-right',
+                itemTextColor: '#444444',
+                itemOpacity: 0.85,
+                symbolSize: 18,
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000000',
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}
+    />}
+        </div>
+    )
+}
+
+export default injectIntl(Chart)
