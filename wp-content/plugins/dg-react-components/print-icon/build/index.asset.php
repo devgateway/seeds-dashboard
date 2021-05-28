e405b276d55815a49699a2dbd881d14c6f242c35
dbianco <dbianco@developmentgateway.org>
2021-05-28 11:46:49 -0300

    Allow to upload larger files

---
+++ wp-content/plugins/dg-react-components/print-icon/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e63de376dc490f34bf713ceb0d4a27bb');
