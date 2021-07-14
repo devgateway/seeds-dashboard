18ffafb59347b9fcb1e96fc79520ec6fbcce5891
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2021-07-14 13:55:31 +0300

    react blocks plugin build adjustment

--- wp-react-blocks-plugin/build_dev.sh
+++ wp-react-blocks-plugin/build_dev.sh
@@ -4,14 +4,10 @@ for d in */; do
    echo ".................. Compiling folder $d ..................."
 
      cd $d
-     cp ../package.json ./package.json
-     cp ../webpack.config.js ./webpack.config.js
      rm -rf build
      pnpm install
      pnpm run build
      # rm -rf node_modules
-     rm webpack.config.js
-     rm package.json
      rm pnpm-lock.yaml
     cd ..
 done
