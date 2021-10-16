fe6d7156c81e9f426fb9a3ea4b205717ab871360
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2021-10-15 21:21:49 -0300

    SEEDSDT-152 custom header

--- ui/src/layout/Header.jsx
+++ ui/src/layout/Header.jsx
@@ -1,142 +1,131 @@
-import {Container, Flag, Menu} from "semantic-ui-react";
-import React, {useEffect, useState} from "react";
-import {MenuConsumer, MenuProvider, utils} from "@devgateway/wp-react-lib";
-import {injectIntl} from "react-intl";
-import {withRouter} from "react-router";
+import { Container, Flag, Menu } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { MenuConsumer, MenuProvider, utils } from "@devgateway/wp-react-lib";
+import { injectIntl } from "react-intl";
+import { withRouter } from "react-router";
 
-
+const MENU_DASHBOARD = 'dashboard';
+const MENU_MAIN = 'main';
 const getPath = (menu, match) => {
-    let path = [];
-    menu.items.forEach(item => {
-        if (item.child_items) {
-            item.child_items.forEach(ch => {
-                if (ch.slug == match.params.slug) {
-                    path.push(item)
-                    path.push(ch)
-                }
-            })
-        } else if (item.slug == match.params.slug && item.url != '/') {
-            path.push(item)
+  let path = [];
+  menu.items.forEach(item => {
+    if (item.child_items) {
+      item.child_items.forEach(ch => {
+        if (ch.slug === match.params.slug) {
+          path.push(item)
+          path.push(ch)
         }
+      })
+    } else if (item.slug === match.params.slug && item.url !== '/') {
+      path.push(item)
+    }
 
 
-    })
-    return path
+  })
+  return path
 }
 
 
-const BreadCrumbs = withRouter(injectIntl(({menu, match, intl}) => {
+const BreadCrumbs = withRouter(injectIntl(({ menu, match, intl }) => {
 
-    let path = getPath(menu, match)
-    return <React.Fragment>
-        <a href={"#"}> Home </a>
-        {path.map(i => !i.child_items ? <a className={i.slug == match.params.slug ? 'active' : ''}
-                                           href={utils.replaceLink(i.url, intl.locale)}> {i.post_title}</a> :
-            <span>{i.post_title} </span>)}
-    </React.Fragment>
+  let path = getPath(menu, match)
+  return <React.Fragment>
+    <a href={"#"}> Home </a>
+    {path.map(i => !i.child_items ? <a className={i.slug === match.params.slug ? 'active' : ''}
+                                       href={utils.replaceLink(i.url, intl.locale)}> {i.post_title}</a> :
+      <span>{i.post_title} </span>)}
+  </React.Fragment>
 
 }))
 
 
 const MyMenuItems = injectIntl(withRouter(({
-                                               withIcons,
-                                               active,
-                                               menu,
-                                               onSetSelected,
-                                               selected,
-                                               match,
-                                               intl: {locale}
+                                             withIcons,
+                                             active,
+                                             menu,
+                                             onSetSelected,
+                                             selected,
+                                             match,
+                                             addClass,
+                                             addSeparator,
+                                             intl: { locale }
                                            }) => {
 
-    useEffect((e) => {
-        if (!selected) {
-            const pathSelected = getPath(menu, match)
-            const items = pathSelected.filter(i => i.menu_item_parent == 0)
-            if (items) {
-                onSetSelected(items[0])
-            }
-        }
+  useEffect(() => {
+    if (!selected) {
+      const pathSelected = getPath(menu, match)
+      const items = pathSelected.filter(i => i.menu_item_parent === 0)
+      if (items) {
+        onSetSelected(items[0])
+      }
+    }
 
-    }, [menu])
+  }, [menu])
 
 
-    return menu && <React.Fragment>
+  return menu && <React.Fragment>
 
-        {menu.items.map(i => (
-            <Menu.Item
-                className={`divided ${i.child_items ? 'has-child-items' : ''} 
-                 ${selected && selected.ID == i.ID ? 'selected' : ''}  ${active == i.slug ? "active" : ""}`}
-            >
+    {menu.items.map((i, index) => (
+      <><Menu.Item
+        className={`divided ${i.child_items ? 'has-child-items' : ''} 
+                 ${selected && selected.ID === i.ID ? 'selected' : ''}  ${active === i.slug ? "active" : ""} 
+                 ${addClass ? i.slug.replace(/\s+/g, '-').toLowerCase() : ''}`}
 
-                {withIcons && <div className={"mark"}></div>} {i.child_items ?
-                <span onMouseOver={e => onSetSelected(i)}>{i.title}</span> :
-                <a onMouseOver={e => onSetSelected(i)} href={utils.replaceLink(i.url, locale)}>{i.title}</a>}
+      >
 
-            </Menu.Item>))}
+        {withIcons && <div className={"mark"} />} {i.child_items ?
+        <span onMouseOver={() => onSetSelected(i)}>{i.title}</span> :
+        <a onMouseOver={() => onSetSelected(i)} href={utils.replaceLink(i.url, locale)}>{i.title}</a>}
 
-    </React.Fragment>
+      </Menu.Item>
+        {index !== menu.items.length - 1 && addSeparator && <Menu.Item fitted />}</>))}
+
+  </React.Fragment>
 }))
 
-const Header = ({intl: {locale} , match}) => {
+const Header = ({ intl: { locale }, match }) => {
 
-    const [selected, setSelected] = useState()
-    const {slug} = match.params
-    const logoUrl = process.env.REACT_APP_USE_HASH_LINKS ? `/#/${locale}` : `/${locale}`
+  const [selected, setSelected] = useState()
+  const { slug, parent } = match.params;
+  let isCustom = false;
+  if ((parent && parent === MENU_DASHBOARD) || slug === MENU_DASHBOARD) {
+    isCustom = true;
+  }
+  const logoUrl = process.env.REACT_APP_USE_HASH_LINKS ? `/#/${locale}` : `/${locale}`
 
-    return <React.Fragment>
+  return <MenuProvider slug={isCustom ? MENU_DASHBOARD : MENU_MAIN}>
+    <Container fluid={true} className="header">
+      <Container fluid={true} className="top_header" />
+      <Container fluid={true} className="background">
+        <Menu className={"branding"} text>
+          <div className="logo-container align-content">
+            <a href={logoUrl}><img alt="Home" className="brand logo" src='/tasai_Logo.svg' /></a>
+          </div>
+          <div className="title-container align-content">
+            <span className="title">TASAI DASHBOARD</span>
+          </div>
+          <div className="lang-container align-content">
+            <div className="lang"><a>français</a></div>
+          </div>
+        </Menu>
+      </Container>
+      <Container fluid={true} className="dashboard-menu">
+        <Menu.Menu className={"pages"} >
+          <MenuConsumer>
+            <MyMenuItems active={slug} selected={selected}
+                         onSetSelected={setSelected} addClass addSeparator />
+          </MenuConsumer>
+        </Menu.Menu>
+      </Container>
+    </Container>
 
-
-        <MenuProvider slug={"main"}>
-            <Container fluid={true} className="header">
-                <Container fluid={true} className={"background"}>
-
-                    <Menu className={"branding"} text>
-                        <Menu.Item>
-                            <a href={logoUrl}><img className="brand logo" size="large" src='/logo_full.png'/></a>
-                        </Menu.Item>
-
-                        <Menu.Item className={"divider"}>
-                            <div></div>
-                        </Menu.Item>
-
-                        <Menu.Item fitted>
-
-                            <Flag name="za"/>
-                            South Africa
-                        </Menu.Item>
-
-                        <Menu.Menu className={"pages"}>
-                            <MenuConsumer>
-                                <MyMenuItems active={slug} selected={selected}
-                                             onSetSelected={setSelected}></MyMenuItems>
-                            </MenuConsumer>
-                        </Menu.Menu>
-
-                        <Menu.Item fitted>
-
-                        </Menu.Item>
-                    </Menu>
-
-                </Container>
-
-                <Container fluid={true} className={"child"}>
-                    {selected && selected.child_items && <Menu fluid text>
-                        <MyMenuItems active={slug} withIcons onSetSelected={e => null}
-                                     menu={{items: selected.child_items}}>}</MyMenuItems>
-
-                    </Menu>}
-                </Container>
-            </Container>
-
-
-            <Container className={"url breadcrumbs"}>
-                <MenuConsumer>
-                    <BreadCrumbs></BreadCrumbs>
-                </MenuConsumer>
-
-            </Container>
-        </MenuProvider>
-    </React.Fragment>
+    {!isCustom && <Container className={"url breadcrumbs"}>
+      <MenuConsumer>
+        <BreadCrumbs />
+      </MenuConsumer>
+    </Container>
+    }
+  </MenuProvider>
 
 }
 
