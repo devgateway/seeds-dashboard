afcf229ec0e708e2426392294eb3552715dcef09
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2021-10-21 15:12:51 -0300

    SEEDSDT-157 regular header, custom header adjustements. styling home page

--- ui/src/layout/ResponsiveContainer.jsx
+++ ui/src/layout/ResponsiveContainer.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-import {Container, Icon, Menu, Sidebar,} from 'semantic-ui-react'
+import { Container, Icon, Menu, Sidebar, } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import MainMenu from "@devgateway/wp-react-lib"
-import {Media} from "../AppMedia"
+import { Media } from "../AppMedia"
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -17,83 +17,83 @@ import Header from "./Header";
  * It can be more complicated, but you can create really flexible markup.
  */
 class DesktopContainer extends Component {
-    render() {
-        const {children, fixed} = this.props
-        return (
-            <Container fluid>
-                <Header></Header>
-                <Container className="desktop">
-                    {children}
-                </Container>
-            </Container>
+  render() {
+    const { children, isHome } = this.props
+    return (
+      <Container fluid>
+        <Header></Header>
+        <Container className={`desktop ${isHome ? ' home' : ''}`} fluid>
+          {children}
+        </Container>
+      </Container>
 
 
-        )
-    }
+    )
+  }
 }
 
 DesktopContainer.propTypes = {
-    children: PropTypes.node,
+  children: PropTypes.node,
 }
 
 class MobileContainer extends Component {
 
-    state = {}
-    handleSidebarHide = () => this.setState({sidebarOpened: false})
-    handleToggle = () => this.setState({sidebarOpened: true})
+  state = {}
+  handleSidebarHide = () => this.setState({ sidebarOpened: false })
+  handleToggle = () => this.setState({ sidebarOpened: true })
 
-    render() {
-        const {children, big} = this.props
-        const {sidebarOpened} = this.state
+  render() {
+    const { children, big } = this.props
+    const { sidebarOpened } = this.state
 
-        return (
-            <Container>
-                <Sidebar
-                    as={Menu}
-                    animation='push'
-                    onHide={this.handleSidebarHide}
-                    vertical
-                    visible={sidebarOpened}>
-                    <Container>
-                        <MainMenu slug="main"/>
-                    </Container>
+    return (
+      <Container>
+        <Sidebar
+          as={Menu}
+          animation='push'
+          onHide={this.handleSidebarHide}
+          vertical
+          visible={sidebarOpened}>
+          <Container>
+            <MainMenu slug="main" />
+          </Container>
 
-                </Sidebar>
+        </Sidebar>
 
-                <Sidebar.Pusher dimmed={sidebarOpened}>
-                    <Container fluid>
-                        <Menu>
-                            <Menu.Item onClick={this.handleToggle}> <Icon name='sidebar' color="orange"/> </Menu.Item>
-                        </Menu>
-                        {children}
-                    </Container>
-                </Sidebar.Pusher>
-            </Container>
-        )
-    }
+        <Sidebar.Pusher dimmed={sidebarOpened}>
+          <Container fluid>
+            <Menu>
+              <Menu.Item onClick={this.handleToggle}> <Icon name='sidebar' color="orange" /> </Menu.Item>
+            </Menu>
+            {children}
+          </Container>
+        </Sidebar.Pusher>
+      </Container>
+    )
+  }
 }
 
 MobileContainer.propTypes = {
-    children: PropTypes.node,
+  children: PropTypes.node,
 }
 
 class ResponsiveContainer extends Component {
 
 
-    render() {
-        const {children, fixed} = this.props
-        return (<div>
-            <style>
-                {Media.mediaStyles}
-            </style>
+  render() {
+    const { children, isHome } = this.props
+    return (<div>
+      <style>
+        {Media.mediaStyles}
+      </style>
 
-            <DesktopContainer fixed={fixed}>
-                {children}
-            </DesktopContainer>
-            <Footer></Footer>
+      <DesktopContainer isHome={isHome}>
+        {children}
+      </DesktopContainer>
+      <Footer></Footer>
 
-        </div>)
-    }
+    </div>)
+  }
 }
 
 
