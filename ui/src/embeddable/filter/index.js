b761c389782b1633f166748e9f16663fef0835f1
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2021-11-03 19:23:33 -0300

    SEEDSDT-168 filters wp plugin

--- ui/src/embeddable/filter/index.js
+++ ui/src/embeddable/filter/index.js
@@ -1,39 +1,36 @@
-import React, {useEffect} from "react";
-import {Container} from "semantic-ui-react";
-import {connect} from "react-redux";
-import {getCategories, setFilter} from "../reducers/data";
-import {DropDownFilter} from './Components'
+import React, { useEffect } from "react";
+import { Container } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { getCategories, setFilter } from "../reducers/data";
+import { CountryFilter } from './Components'
 import './filter.scss'
 
 const Filter = ({
-                    onApply, "data-type": type = 'AgeGroup', "data-param": param,
-                    "data-placeholder": placeholder, categories, onLoadCategories
+                  onApply, "data-type": type = 'AgeGroup', categories, onLoadCategories
                 }) => {
-    useEffect(() => {
-        onLoadCategories()
-    }, [])
+  /*useEffect(() => {
+    onLoadCategories()
+  }, [])*/
+  /*TODO to be complented in SEEDSDT-170*/
+  /*return <Container fluid={true} className={"filter"}>
+    <CountryFilter type={type} onChange={(e, { value }) => {
+      onApply(param, value)
+    }}></CountryFilter>
 
-    if (categories) {
-
-
-    }
-    return <Container fluid={true} className={"filter"}>
-        <DropDownFilter placeholder={placeholder} categories={categories} type={type} onChange={(e, {value}) => {
-            onApply(param, value)
-        }}></DropDownFilter>
-
-    </Container>
+  </Container>*/
+  return <Container fluid={true} className={"filters"}><CountryFilter /></Container>
 }
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        categories: state.getIn(['data', 'categories'])
-    }
+  return {
+    categories: state.getIn(['data', 'categories'])
+  }
 }
 
 const mapActionCreators = {
-    onApply: setFilter,
-    onLoadCategories: getCategories
+  onApply: setFilter,
+  onLoadCategories: getCategories
 };
 
-export default connect(mapStateToProps, mapActionCreators)(Filter)
+//export default connect(mapStateToProps, mapActionCreators)(Filter)
+export default Filter;
--- wp-react-blocks-plugin/blocks/filter/BlockSave.js
+++ wp-react-blocks-plugin/blocks/filter/BlockSave.js
@@ -1,36 +1,27 @@
-import {useBlockProps} from '@wordpress/block-editor';
+import { useBlockProps } from '@wordpress/block-editor';
 
 const SaveComponent = (props) => {
 
-    const {
-        attributes: {
-            placeHolder,
-            type,
-            param
-        }
-    } = props;
-    const blockProps = useBlockProps.save({
-        className: 'wp-react-lib component chart'
-    });
+  const {
+    attributes: {
+      type
+    }
+  } = props;
+  const blockProps = useBlockProps.save({
+    className: 'wp-react-lib component chart'
+  });
 
-    const divClass = {}
-    const divStyles = {}
-
-
-    return (
-        <div className={"wp-react-lib-component"}
-             data-component={"filter"}
-
-             data-type={type}
-             data-param={param}
-             data-placeholder={placeHolder}
-        >
+  const divClass = {}
+  const divStyles = {}
+  return (
+    <div className={"wp-react-lib-component"}
+         data-component={"filter"}
+         data-type={type}
+    >
+    </div>
 
 
-        </div>
-
-
-    );
+  );
 }
 
 
