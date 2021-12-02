727f360d7e882d996b43caa4e1b027b8b6be0934
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2021-12-02 15:10:38 -0300

    SEEDSDT-216 allow multiple posts in post carrousel

--- wp-react-blocks-plugin/blocks/post-carousel/BlockEdit.js
+++ wp-react-blocks-plugin/blocks/post-carousel/BlockEdit.js
@@ -1,100 +1,113 @@
-import {InspectorControls, useBlockProps} from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import {
-    __experimentalNumberControl as NumberControl,
-    Panel,
-    PanelBody,
-    PanelRow,
-    ResizableBox,
+  __experimentalNumberControl as NumberControl,
+  Panel,
+  PanelBody,
+  PanelRow,
+  ResizableBox,
 } from '@wordpress/components';
-import {__} from '@wordpress/i18n';
-import {BlockEditWithFilters, SizeConfig} from "../commons";
+import { __ } from '@wordpress/i18n';
+import { BlockEditWithFilters, SizeConfig } from "../commons";
 
 
 class BlockEdit extends BlockEditWithFilters {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        const {
-            className, isSelected,
-            toggleSelection,
-            setAttributes,
-            attributes: {
-                count,
-                type,
-                taxonomy,
-                categories,
-                height=650
-            },
-        } = this.props;
-
-
-        const queryString = `editing=true&data-type=${type}&data-taxonomy=${taxonomy}&data-categories=${categories}&data-items=${count}`
-        const divStyles = {height: height+'px', width: '100%'}
-
-        return (
-            <div>
-                <InspectorControls>
-                    <Panel header={__("Carousel Configuration")}>
-                        <PanelBody>
-                            <PanelRow>
-                                <NumberControl
-                                    isShiftStepEnabled={true}
-                                    onChange={(count) => setAttributes({count})}
-                                    shiftStep={10}
-                                    value={count}
-                                    label={__("Items")}/>
-                            </PanelRow>
-
-                        </PanelBody>
-                        <SizeConfig initialOpen={false} setAttributes={setAttributes} height={height}></SizeConfig>
-                        {this.renderFilters()}
-                    </Panel>
-                </InspectorControls>
+  render() {
+    const {
+      className, isSelected,
+      toggleSelection,
+      setAttributes,
+      attributes: {
+        count,
+        type,
+        taxonomy,
+        categories,
+        height = 650,
+        itemsPerPage
+      },
+    } = this.props;
 
 
-                <ResizableBox
-                    size={{height}}
-                    style={{"margin": "auto", width: "100%"}}
-                    minHeight="200"
-                    minWidth="100"
-                    enable={{
-                        top: false,
-                        right: false,
-                        bottom: true,
-                        left: false,
-                        topRight: true,
-                        bottomRight: false,
-                        bottomLeft: false,
-                        topLeft: false,
-                    }}
-                    onResizeStop={(event, direction, elt, delta) => {
-                        setAttributes({
-                            height: parseInt(height + delta.height, 10),
-                        });
-                        toggleSelection(true);
-                    }}
-                    onResizeStart={() => {
-                        toggleSelection(false);
-                    }}>
-                    <div style={divStyles}>
-                        <iframe style={divStyles} scrolling={"no"}
-                                src={this.state.react_ui_url + "/en/embeddable/postscarousel?" + queryString}/>
-                    </div>
-                </ResizableBox>
+    let queryString = `editing=true&data-type=${type}`
+    queryString += `&data-taxonomy=${taxonomy}`;
+    queryString += `&data-categories=${categories}`;
+    queryString += `&data-items=${count}`;
+    queryString += `&data-items-per-page=${itemsPerPage}`;
+
+    const divStyles = { height: height + 'px', width: '100%' }
+
+    return (
+      <div>
+        <InspectorControls>
+          <Panel header={__("Carousel Configuration")}>
+            <PanelBody>
+              <PanelRow>
+                <NumberControl
+                  isShiftStepEnabled={true}
+                  onChange={(count) => setAttributes({ count })}
+                  shiftStep={10}
+                  value={count}
+                  label={__("Items")} />
+              </PanelRow>
+              <PanelRow>
+                <NumberControl
+                  isShiftStepEnabled={true}
+                  onChange={(itemsPerPage) => setAttributes({ itemsPerPage })}
+                  shiftStep={10}
+                  value={itemsPerPage}
+                  label={__("items per page")} />
+              </PanelRow>
+            </PanelBody>
+            <SizeConfig initialOpen={false} setAttributes={setAttributes} height={height}></SizeConfig>
+            {this.renderFilters()}
+          </Panel>
+        </InspectorControls>
 
 
-            </div>
-        );
+        <ResizableBox
+          size={{ height }}
+          style={{ "margin": "auto", width: "100%" }}
+          minHeight="200"
+          minWidth="100"
+          enable={{
+            top: false,
+            right: false,
+            bottom: true,
+            left: false,
+            topRight: true,
+            bottomRight: false,
+            bottomLeft: false,
+            topLeft: false,
+          }}
+          onResizeStop={(event, direction, elt, delta) => {
+            setAttributes({
+              height: parseInt(height + delta.height, 10),
+            });
+            toggleSelection(true);
+          }}
+          onResizeStart={() => {
+            toggleSelection(false);
+          }}>
+          <div style={divStyles}>
+            <iframe style={divStyles} scrolling={"no"}
+                    src={this.state.react_ui_url + "/en/embeddable/postscarousel?" + queryString} />
+          </div>
+        </ResizableBox>
 
-    }
+
+      </div>
+    );
+
+  }
 }
 
 const Edit = (props) => {
-    const blockProps = useBlockProps({className: 'wp-react-component'});
-    return <div {...blockProps}><BlockEdit {...props}/></div>;
+  const blockProps = useBlockProps({ className: 'wp-react-component' });
+  return <div {...blockProps}><BlockEdit {...props} /></div>;
 
 }
 export default Edit;
--- wp-react-blocks-plugin/blocks/post-carousel/BlockSave.js
+++ wp-react-blocks-plugin/blocks/post-carousel/BlockSave.js
@@ -1,27 +1,31 @@
 const SaveComponent = (props) => {
-    const {
-        setAttributes,
-        attributes: {
-            count,
-            type,
-            taxonomy,
-            categories
-        },
-    } = props;
+  const {
+    setAttributes,
+    attributes: {
+      count,
+      type,
+      taxonomy,
+      categories,
+      itemsPerPage
+    },
+  } = props;
 
-    const divClass = {}
-    const divStyles = {}
-
-
-    return (<div className={divClass} style={divStyles}>
-            <div data-items={count} data-type={type} data-taxonomy={taxonomy} data-categories={categories.toString()}
-                 className={"wp-react-lib-component"}
-                 data-component={"postsCarousel"}>
-            </div>
-        </div>
+  const divClass = {}
+  const divStyles = {}
 
 
-    );
+  return (<div className={divClass} style={divStyles}>
+      <div data-items={count} data-type={type}
+           data-items-per-page={itemsPerPage}
+           data-taxonomy={taxonomy}
+           data-categories={categories.toString()}
+           className={"wp-react-lib-component"}
+           data-component={"postsCarousel"}>
+      </div>
+    </div>
+
+
+  );
 }
 
 
