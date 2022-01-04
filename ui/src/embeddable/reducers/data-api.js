ccb81b5b4ab91d77dda7d619adb063c26ce3121e
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2022-01-04 13:06:35 -0300

    SEEDSDT-245 Connect with filters - Fix merge issue

--- ui/src/embeddable/reducers/data-api.js
+++ ui/src/embeddable/reducers/data-api.js
@@ -14,8 +14,7 @@ const SURVEY_INDICATORS_API = `${SURVEY_API}/${SURVEY_FILTER_API}/indicators`
 const SURVEY_INDICATOR_INFORMATION_API = `${SURVEY_API}/${DATA_SUMMARY}/categoryId/{categoryId}/latest`;
 const WP_DOCUMENTS_API = `${APP_WP_API}/wp/v2/media`;
 
-//TODO remove hardcoded country once SEEDSDT-243 is done
-let COUNTRY_INFORMATION_API = `${SURVEY_API}/countryInfo/countryId/9`;
+let COUNTRY_INFORMATION_API = `${SURVEY_API}/countryInfo/countryId/`;
 const APIS = {
   prevalence: '',
   policy: POLICY_API_ROOT,
