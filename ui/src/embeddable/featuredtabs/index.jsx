f813d990161ba2f68246a7c2b96affca9bef9f76
Daniel Oliva <dbianco@users.noreply.github.com>
2022-02-04 10:35:31 -0300

    Update index.jsx
    remove debugger

--- ui/src/embeddable/featuredtabs/index.jsx
+++ ui/src/embeddable/featuredtabs/index.jsx
@@ -11,7 +11,6 @@ import {
     PostTitle
 } from "@devgateway/wp-react-lib";
 
-debugger
 
 const FeaturedPost = ({post, onClick, active, moreLabel}) => {
     const media = post['_embedded'] ? post['_embedded']["wp:featuredmedia"] : null
