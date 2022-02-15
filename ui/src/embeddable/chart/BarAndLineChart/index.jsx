0b34d9a6085148b81ce2903b35fc1adf2bc7c6b9
ginchauspe <ginchauspe@gmail.com>
2022-02-15 17:29:10 -0300

    SEEDSDT-544: Fix problem with 1 line chart.

--- ui/src/embeddable/chart/BarAndLineChart/index.jsx
+++ ui/src/embeddable/chart/BarAndLineChart/index.jsx
@@ -171,7 +171,7 @@ const BarAndLineChart = ({
         markerLine = [
             {
                 axis: 'y',
-                value: max > FAKE_NUMBER ? percent * max / 100 : percent * 10000 / 100,
+                value: max > FAKE_NUMBER ? percent * newMax / 100 : percent * 10000 / 100,
                 lineStyle: {stroke: lineColor, strokeWidth: 2},
                 legend: '',
                 legendOrientation: 'vertical',
