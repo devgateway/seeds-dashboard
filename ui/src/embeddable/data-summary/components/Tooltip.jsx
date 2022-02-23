c03d0baeca4908d9b4516c8459a6cb563f9ec2e1
ginchauspe <ginchauspe@gmail.com>
2022-02-23 17:34:45 -0300

    SEEDSDT-254: Hide info icon if tooltip text has not been defined.

--- ui/src/embeddable/data-summary/components/Tooltip.jsx
+++ ui/src/embeddable/data-summary/components/Tooltip.jsx
@@ -1,16 +1,22 @@
-import { injectIntl } from "react-intl";
-import { Icon, Popup } from "semantic-ui-react";
+import {injectIntl} from "react-intl";
+import {Icon, Popup} from "semantic-ui-react";
 import React from "react";
 
-const Tooltip = ({ item, intl, tiny }) => {
-  return <Popup
-    trigger={<Icon circular size={tiny ? 'tiny' : 'small'} name='info' />}
-    className="indicator-popup"
-    position="right center">
-    {intl.formatMessage({
-      id: `tooltip-${item.key}`,
-      defaultMessage: 'Missing key' + item.key
-    })}
-  </Popup>
+const Tooltip = ({item, intl, tiny}) => {
+    const EMPTY = 'EMPTY';
+    const key = `tooltip-${item.key}`;
+    const test = intl.formatMessage({id: key, defaultMessage: EMPTY});
+    if (test === EMPTY) {
+        return null;
+    }
+    return <Popup
+        trigger={<Icon circular size={tiny ? 'tiny' : 'small'} name='info'/>}
+        className="indicator-popup"
+        position="right center">
+        {intl.formatMessage({
+            id: key,
+            defaultMessage: 'Missing key' + item.key
+        })}
+    </Popup>
 }
 export default injectIntl(Tooltip);
