50c30a1e66e87c85f505044aec450cafc1e34aba
ginchauspe <ginchauspe@gmail.com>
2022-02-25 10:07:49 -0300

    SEEDSDT-610: Temporarily disable share link.

--- ui/src/embeddable/chart/common/export/index.jsx
+++ ui/src/embeddable/chart/common/export/index.jsx
@@ -9,11 +9,12 @@ const Export = ({methodology, download, exportPng, containerRef, type}) => {
                 {download === 'true'
                     ? <div className="export download" onClick={e => exportPng(containerRef, type)}/>
                     : null}
-                <div className="export share"/>
+                {/*<div className="export share"/>*/}
             </div>
             {methodology
-                ? <Popup className="methods-popup" content={methodology} trigger={<div className="tooltip">Methods</div>}
-                         position='bottom right'/>
+                ?
+                <Popup className="methods-popup" content={methodology} trigger={<div className="tooltip">Methods</div>}
+                       position='bottom right'/>
                 : null}
         </div>
     )
