8820b84796c443e4ce84c677188eb994c66672b2
ginchauspe <ginchauspe@gmail.com>
2022-03-08 10:23:11 -0300

    SEEDSDT-642: UI changes.

--- ui/src/embeddable/country-reports/index.js
+++ ui/src/embeddable/country-reports/index.js
@@ -1,26 +1,32 @@
 import React, {useEffect} from "react";
-import {Container} from "semantic-ui-react";
+import {Container, Grid, GridColumn, GridRow} from "semantic-ui-react";
 import {connect} from "react-redux";
-import {getDocuments, getWpCategories} from "../reducers/data";
-import {DATA, SELECTED_COUNTRY, WP_CATEGORIES, WP_DOCUMENTS, DATA_CATEGORY} from "../reducers/StoreConstants";
+import {getDocuments, getImages, getWpCategories} from "../reducers/data";
+import {
+    DATA, SELECTED_COUNTRY, WP_CATEGORIES, WP_DOCUMENTS, DATA_CATEGORY, WP_IMAGES
+} from "../reducers/StoreConstants";
+import './styles.scss';
+import CropsLegend from "../chart/common/crop";
 
 const DOCUMENTS_PER_PAGE = 100;
 
 const CountryReports = ({
                             onLoadDocuments,
                             onLoadCategories,
+                            onLoadImages,
                             documents,
                             loading,
                             error,
-                            "data-type": dataType,
-                            "data-show-inline": showInline = true,
+                            "data-description": description,
+                            "data-country": country,
                             categoriesWP,
                             selectedCountry: selectedCountryId,
-                            countries,
-                            "data-category": category,
-                            "data-no-data-text": noDataText,
-                            "data-document-slug-post-fix": documentSlugPostFix,
-                            editing
+                            "data-language": language,
+                            "data-year": year,
+                            "data-image": image,
+                            editing,
+                            "data-width": width,
+                            "data-height": height,
                         }) => {
     useEffect(() => {
         onLoadCategories()
@@ -29,32 +35,52 @@ const CountryReports = ({
     useEffect(() => {
         if (categoriesWP && !loading) {
             const params = {};
-            params.categories = category;
             params.per_page = DOCUMENTS_PER_PAGE;
             onLoadDocuments({params})
         }
     }, [categoriesWP, onLoadDocuments]);
 
+    useEffect(() => {
+        const params = {};
+        params.per_page = DOCUMENTS_PER_PAGE;
+        onLoadImages({params})
+    }, [onLoadImages]);
+
     const classes = 'styles reports';
-    if (editing === 'true') {
-        return <Container fluid={true} className={classes}>
-            <span>The list of documents is available in preview mode only.</span>
-        </Container>
-    }
+    let childComponent = null;
 
-    let filtered;
-    // Match country in the media categories with country in the data filter component.
-    if (documents && selectedCountryId && countries && category !== '0') {
-        const selectedCountry = countries.find(i => i.countryId === selectedCountryId);
-        const countryCategory = categoriesWP.find(i => {
-
-            return i.slug.toLowerCase() === (selectedCountry.country.toLowerCase().replace(/\s+/g, '-').toLowerCase() + documentSlugPostFix) && i.parent === Number(category);
-        })
-        if (countryCategory) {
-            filtered = documents.filter(i => i.categories.find(j => j === countryCategory.id));
-        }
+    if (!loading && categoriesWP && year && country && language) {
+        const year_ = categoriesWP.find(i => i.id === Number(year));
+        const country_ = categoriesWP.find(i => i.id === Number(country));
+        const language_ = categoriesWP.find(i => i.id === Number(language));
+        childComponent = (<div className="box" style={{width: width + 'px', height: height + 'px'}}>
+            <Grid>
+                <GridRow>
+                    <GridColumn width={5}>
+                        <div className="image">
+                            <img
+                                src="https://www.researchgate.net/profile/Zahir-Dehouche/publication/324114338/figure/fig1/AS:621209800671234@1525119366926/Pictures-of-nine-common-Nigerian-food-crops-and-their-various-waste-products.png"
+                                width='180px'
+                            />
+                        </div>
+                    </GridColumn>
+                    <GridColumn width={11}>
+                        <div>
+                            <div className="crops">
+                                <CropsLegend data={['maize', 'bean', 'rice', 'sorghum']}/>
+                            </div>
+                            <div className="report-container">
+                                <span className="title">{country_.name + ' ' + year_.name + ' Report'}</span>
+                                <span className="description">{description}</span>
+                            </div>
+                        </div>
+                    </GridColumn>
+                </GridRow>
+            </Grid>
+        </div>);
+    } else {
+        childComponent = (<div>Loading...</div>);
     }
-    const childComponent = <div>nada</div>;
     return <Container fluid={true} className={classes}>{childComponent}</Container>
 }
 
@@ -65,12 +91,12 @@ const mapStateToProps = (state, ownProps) => {
         error: state.getIn([DATA, ownProps[DATA_CATEGORY], WP_DOCUMENTS, 'error']),
         categoriesWP: state.getIn([DATA, WP_CATEGORIES]),
         selectedCountry: state.getIn([DATA, 'filters', SELECTED_COUNTRY]),
-        countries: state.getIn([DATA, 'countries'])
+        images: state.getIn([DATA, ownProps[DATA_CATEGORY], WP_IMAGES, 'data']),
     }
 }
 
 const mapActionCreators = {
-    onLoadDocuments: getDocuments, onLoadCategories: getWpCategories
+    onLoadDocuments: getDocuments, onLoadCategories: getWpCategories, onLoadImages: getImages
 };
 
 export default connect(mapStateToProps, mapActionCreators)(CountryReports)
