16348395c0e6b0cd66c48ac0f9de53fcb00fb8ef
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2022-03-16 10:37:32 -0300

    SEEDSDT-705 enable server side routing

--- wp-react-lib/src/util/index.js
+++ wp-react-lib/src/util/index.js
@@ -12,6 +12,7 @@ export const replaceLink = (url, locale, isAddTypeToLink) => {
         type = "type/";
     }
     let finalUrl;
+    debugger
     if (useHash) {
         if (url.includes(`/${locale}/`)) {
             finalUrl = url.replaceAll(all, `/#${type}`);
@@ -20,7 +21,11 @@ export const replaceLink = (url, locale, isAddTypeToLink) => {
         }
 
     } else {
-        finalUrl = url.replaceAll(all, "" + locale + type)
+        if (url.includes(`/${locale}/`)) {
+            finalUrl = url.replaceAll(all, `${type}`)
+        } else {
+            finalUrl = url.replaceAll(all, `${locale}${type}`)
+        }
     }
     return finalUrl;
 }
@@ -41,7 +46,11 @@ export const replaceHTMLinks = (html, locale) => {
                 newLink = href.replace(all, `#/${locale}`) //TODO:fix it!
             }
         } else {
-            newLink = href.replace(all, '' + locale) //TODO:fix it!
+            if (href.includes(`/${locale}/`)) {
+                newLink = href.replace(all, '') //TODO:fix it!
+            } else {
+                newLink = href.replace(all, '' + locale) //TODO:fix it!
+            }
         }
         newHtml = newHtml.replaceAll(link[2], newLink)
     }
