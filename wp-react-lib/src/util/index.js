ae70a0bf5edf1a319876930c3c140baff8bccfec
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2022-03-16 14:41:18 -0300

    SEEDSDT-705 fix url

--- wp-react-lib/src/util/index.js
+++ wp-react-lib/src/util/index.js
@@ -26,7 +26,6 @@ export const replaceLink = (url, locale, isAddTypeToLink) => {
             finalUrl = url.replaceAll(all, `/${locale}${type}`)
         }
     }
-    debugger
     return finalUrl;
 }
 
