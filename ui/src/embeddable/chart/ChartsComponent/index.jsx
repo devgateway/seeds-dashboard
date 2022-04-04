2692cd0ef3367326af280967f31b0e40b9db7e59
dbianco <dbianco@developmentgateway.org>
2022-04-04 16:06:46 -0300

    SEEDSDT-704 fix

--- ui/src/embeddable/chart/ChartsComponent/index.jsx
+++ ui/src/embeddable/chart/ChartsComponent/index.jsx
@@ -142,7 +142,7 @@ const ChartComponent = ({ sources, data, type, title, subTitle, editing, intl, m
         const yearObject = { year: y };
         let maxByYear = 0;
         crops.forEach(c => {
-          if (data.values[y][c]) {
+          if (data.values[y][c] && !isNaN(data.values[y][c])) {
             maxByYear += data.values[y][c];
             const objKey = y + "_" + c;
             yearObject[objKey] = data.values[y][c];
