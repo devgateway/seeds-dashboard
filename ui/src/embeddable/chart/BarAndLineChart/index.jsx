2f0719bb1ff3e1bff7bfbde131823bffbb251788
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2022-04-20 15:22:21 -0300

    SEEDSDT-356 scroll to top on indicator click

--- ui/src/embeddable/chart/BarAndLineChart/index.jsx
+++ ui/src/embeddable/chart/BarAndLineChart/index.jsx
@@ -1,13 +1,13 @@
-import React, {Fragment} from "react";
-import {Grid} from "semantic-ui-react";
-import {line} from "d3-shape";
-import {BasicTooltip, useTooltip} from '@nivo/tooltip';
+import React, { Fragment } from "react";
+import { Grid } from "semantic-ui-react";
+import { line } from "d3-shape";
+import { useTooltip } from '@nivo/tooltip';
 import * as d3 from 'd3';
 import './styles.scss';
 import ResponsiveBarChartImpl from "../ResponsiveBarChartImpl";
 import Legend from "./Legend";
-import {FAKE_NUMBER} from "../ChartsComponent";
-import {getTextWidth} from "../../utils/common";
+import { FAKE_NUMBER } from "../ChartsComponent";
+import { getTextWidth } from "../../utils/common";
 import NoData from "../common/noData";
 
 const round = (x, y) => {
@@ -44,7 +44,7 @@ const BarAndLineChart = ({
     if (noData) {
         return (<Grid.Row className="chart-section">
             <Grid.Column width={16}>
-                <NoData noDataLabelId={noDataLabelId}/>
+                <NoData noDataLabelId={noDataLabelId} />
             </Grid.Column>
         </Grid.Row>);
     }
@@ -58,7 +58,7 @@ const BarAndLineChart = ({
     const incrementalFactor = Number('1' + ''.padStart(String(max).length - 1, '0'));
     const newMax = round(max, incrementalFactor);
 
-    const LineLayer = ({bars, xScale, yScale, innerWidth}) => {
+    const LineLayer = ({ bars, xScale, yScale, innerWidth }) => {
         const filterIndex = [];
         const filteredBars = bars.filter(b => {
             if (b.data.data[lineChartField] !== FAKE_NUMBER) {
@@ -76,7 +76,7 @@ const BarAndLineChart = ({
             .x(bar => xScale(bar.data.indexValue) + bar.width / 2)
             .y(bar => newYScale(bar.data.data[lineChartField] || 0));
 
-        const {showTooltipFromEvent, hideTooltip} = useTooltip();
+        const { showTooltipFromEvent, hideTooltip } = useTooltip();
         const chartHeight = yScale.range()[0];
 
         return (
@@ -86,7 +86,7 @@ const BarAndLineChart = ({
                     fill="none"
                     stroke={lineColor}
                     strokeWidth={2}
-                    style={{pointerEvents: "none"}}
+                    style={{ pointerEvents: "none" }}
                 />
                 {selectedYear.length > 0 && bars.map(bar => {
                     if (selectedYear.find(i => i === bar.data.indexValue)
@@ -105,7 +105,7 @@ const BarAndLineChart = ({
                                         <div className="header-container">
                                             <div className="header">
                                                 <div className="inner-container">
-                                                    <div className="crop-icon"/>
+                                                    <div className="crop-icon" />
                                                     <div className="crop-name">{bar.data.indexValue}</div>
                                                 </div>
                                             </div>
@@ -115,7 +115,7 @@ const BarAndLineChart = ({
                                                 <tbody>
                                                 <tr>
                                                     <td>
-                                                        <div style={{textAlign: 'center'}}>
+                                                        <div style={{ textAlign: 'center' }}>
                                                             <span>{lineChartFieldLabel}</span><span
                                                             className="bold"> {bar.data.data[lineChartField] !== FAKE_NUMBER ? bar.data.data[lineChartField] : "MD"}</span>
                                                         </div>
@@ -144,7 +144,7 @@ const BarAndLineChart = ({
                 })}
                 <text
                     transform={`translate(${innerWidth + 50}, ${chartHeight - ((chartHeight - getTextWidth(rightLegend, '16px sans-serif')) / 2)}) rotate(-90)`}
-                    style={{fontFamily: 'Lato', fontSize: '16px', fill: '#354052', fontWeight: 400}}>
+                    style={{ fontFamily: 'Lato', fontSize: '16px', fill: '#354052', fontWeight: 400 }}>
                     {rightLegend}
                 </text>
             </Fragment>
@@ -159,7 +159,7 @@ const BarAndLineChart = ({
                 {
                     axis: 'y',
                     value: percent * newMax / 100,
-                    lineStyle: {stroke: lineColor, strokeWidth: 2},
+                    lineStyle: { stroke: lineColor, strokeWidth: 2 },
                     legend: '',
                     legendOrientation: 'vertical',
                 }
@@ -173,7 +173,7 @@ const BarAndLineChart = ({
             {
                 axis: 'y',
                 value: max > FAKE_NUMBER ? percent * newMax / 100 : percent * 10000 / 100,
-                lineStyle: {stroke: lineColor, strokeWidth: 2},
+                lineStyle: { stroke: lineColor, strokeWidth: 2 },
                 legend: '',
                 legendOrientation: 'vertical',
             }
@@ -190,7 +190,7 @@ const BarAndLineChart = ({
     return (
         <>
             <Grid.Row className={`hhi-section`}>
-                <Legend legends={legends} title={'Legend'}/>
+                <Legend legends={legends} title={'Legend'} />
             </Grid.Row>
             <Grid.Row className="chart-section">
                 <Grid.Column width={16}>
