6ace59a993d4a5e82a63afdb920766dfb3f473d0
gkalvatcheva <gkalvatcheva@developmentgateway.org>
2022-05-26 21:56:29 -0400

    SEEDSDT-837
    mobile message adjustment

--- ui/src/layout/MobileMessage.jsx
+++ ui/src/layout/MobileMessage.jsx
@@ -3,7 +3,7 @@ import { Container } from "semantic-ui-react";
 
 const MobileMessage = () => {
   return <Container>
-  <div className="mobile-message">Test message</div>
+  <div className="mobile-message">This website is best viewed on desktop.</div>
   </Container>
 }
 export default MobileMessage
