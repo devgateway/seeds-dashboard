c7f954fb5bdcd4f0977b7b7a7685cc71d41f05ac
ginchauspe <ginchauspe@gmail.com>
2022-07-11 14:28:29 -0300

    SEEDSDT-890: Add chart D1.7

--- ui/src/embeddable/chart/index.jsx
+++ ui/src/embeddable/chart/index.jsx
@@ -47,7 +47,8 @@ import {
     CROSS_COUNTRY_MARKET_SHARE_TOP_FOUR_SEED_COMPANIES,
     CROSS_COUNTRY_MARKET_CONCENTRATION_HHI,
     CROSS_COUNTRY_MARKET_SHARE_STATE_OWNED_SEED_COMPANIES,
-    CROSS_COUNTRY_VARIETY_RELEASE_PROCESS
+    CROSS_COUNTRY_VARIETY_RELEASE_PROCESS,
+    CROSS_COUNTRY_OVERALL_RATING_NATIONAL_SEED_TRADE_ASSOCIATION
 } from "../reducers/StoreConstants";
 import GaugesChart from "./GaugesChart";
 import { getWpCategories, setFilter } from "../reducers/data";
@@ -288,7 +289,8 @@ const Chart = (props) => {
         case CROSS_COUNTRY_MARKET_SHARE_TOP_FOUR_SEED_COMPANIES:
         case CROSS_COUNTRY_MARKET_CONCENTRATION_HHI:
         case CROSS_COUNTRY_MARKET_SHARE_STATE_OWNED_SEED_COMPANIES:
-        case CROSS_COUNTRY_VARIETY_RELEASE_PROCESS: {
+        case CROSS_COUNTRY_VARIETY_RELEASE_PROCESS:
+        case CROSS_COUNTRY_OVERALL_RATING_NATIONAL_SEED_TRADE_ASSOCIATION: {
             const chartComponent = { type, ...chartProps }
             child = <ChartComponent {...chartComponent} sources={dynamicSources} />
             break;
@@ -351,6 +353,7 @@ const Chart = (props) => {
         [CROSS_COUNTRY_MARKET_CONCENTRATION_HHI]: 875,
         [CROSS_COUNTRY_MARKET_SHARE_STATE_OWNED_SEED_COMPANIES]: 875,
         [CROSS_COUNTRY_VARIETY_RELEASE_PROCESS]: 875,
+        [CROSS_COUNTRY_OVERALL_RATING_NATIONAL_SEED_TRADE_ASSOCIATION]: 875
     };
     const fixedHeightStyle = { height: (fixedHeight[type] ? fixedHeight[type] : 550) + 'px' };
 
