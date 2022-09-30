3bc6684833c9b21e1c4d754f807910d9241cdea9
ginchauspe <ginchauspe@gmail.com>
2022-09-30 17:52:20 -0300

    SEEDSDT-901: Basic map component config for WP.

---
+++ wp-react-blocks-plugin/blocks/maps/BlockSave.js
@@ -0,0 +1,30 @@
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks } from '@wordpress/editor';
+
+const SaveComponent = (props) => {
+    const {
+        toggleSelection, setAttributes, attributes: {
+            type,
+            title_en,
+            title_fr,
+            sourceText_en,
+            sourceText_fr,
+        }
+    } = props;
+    const blockProps = useBlockProps.save({
+        className: 'wp-react-lib component map'
+    });
+    const divClass = {}
+    const divStyles = {}
+    return (
+        <div className={"wp-react-lib-component"}
+             data-component={"map"}
+             data-map-type={type}
+             data-title_en={title_en}
+             data-source-text_en={encodeURI(sourceText_en)}
+             data-source-text_fr={encodeURI(sourceText_fr)}
+        />
+    );
+}
+
+export default SaveComponent
