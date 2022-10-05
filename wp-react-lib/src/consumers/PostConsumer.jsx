a48b9925372b2ed22915d4f15a4abd0ac8719b05
Julian de Anquin <jdeanquin@developmentgateway.org>
2022-10-05 16:22:37 -0300

    SEEDSDT-901 fix translations for posts

--- wp-react-lib/src/consumers/PostConsumer.jsx
+++ wp-react-lib/src/consumers/PostConsumer.jsx
@@ -4,9 +4,14 @@ import {PostContext} from '../providers/Context'
 const PostConsumer = (props) => {
     return (
         <PostContext.Consumer>
-            {({posts, meta, locale}) => {
+            {({posts, meta, locale, messages}) => {
                 return posts && <React.Fragment>
-                    {React.Children.map(props.children, (child => React.cloneElement(child, {posts, meta, locale})))}
+                    {React.Children.map(props.children, (child => React.cloneElement(child, {
+                        posts,
+                        meta,
+                        locale,
+                        messages
+                    })))}
                 </React.Fragment>
             }}
         </PostContext.Consumer>
--- wp-react-lib/src/providers/PostProvider.jsx
+++ wp-react-lib/src/providers/PostProvider.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { getPosts } from '../reducers/actions'
-import { PostContext } from './Context'
-import { Container, Loader, Segment } from "semantic-ui-react";
+import {connect} from 'react-redux'
+import {getPosts} from '../reducers/actions'
+import {PostContext} from './Context'
+import {Container, Loader, Segment} from "semantic-ui-react";
 import LocalizedProvider from "./LocalizedProvider"
 
 class PostProvider extends React.Component {
@@ -79,7 +79,7 @@ class PostProvider extends React.Component {
     }
 
     render() {
-        const { posts, meta, loading, error, locale, isScheduledFilter, scheduledFilterStore } = this.props;
+        const {posts, meta, loading, error, locale, isScheduledFilter, scheduledFilterStore, messages} = this.props;
         if (posts && (posts.length > 0 || posts.id)) {
             let postsArray = posts;
             if (!Array.isArray(postsArray)) {
@@ -112,9 +112,8 @@ class PostProvider extends React.Component {
                 postsArray = postsArray.sort((a, b) => !a.date || !b.date ? 0
                     : new Date(b.date) - new Date(a.date));
             }
-
             return <PostContext.Provider
-                value={{ posts: postsArray, locale, meta }}>{this.props.children}</PostContext.Provider>
+                value={{posts: postsArray, locale, meta, messages}}>{this.props.children}</PostContext.Provider>
         } else if (error) {
             return <Segment color={"red"}>
                 <h1>500</h1>
@@ -123,7 +122,7 @@ class PostProvider extends React.Component {
         } else if (loading) {
             return (<Container>
                 <span>Loading...</span>
-                <Loader inverted content='Loading...' />
+                <Loader inverted content='Loading...'/>
             </Container>)
         } else {
             return <Container>
@@ -136,7 +135,7 @@ class PostProvider extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-    const { store = "posts" } = ownProps
+    const {store = "posts"} = ownProps
     return {
         meta: state.getIn(['wordpress', store, 'meta']),
         posts: state.getIn(['wordpress', store, 'items']),
