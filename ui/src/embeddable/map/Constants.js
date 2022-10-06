0e793d881c7052c413db8a9b1549d9543654a39c
ginchauspe <ginchauspe@gmail.com>
2022-10-06 15:20:03 -0300

    SEEDSDT-904: Add map.

--- ui/src/embeddable/map/Constants.js
+++ ui/src/embeddable/map/Constants.js
@@ -3,3 +3,6 @@ export const A4_AVAILABILITY_FOUNDATION_SEED = "A4. Availability of foundation (
 export const D2_ADEQUACY_SEED_INSPECTION_SERVICES = "D2. Adequacy of seed inspection services (opinion)";
 export const E13_ADEQUACY_EXTENSION_SERVICES = "E1.3 Adequacy of extension services";
 export const E24_ADEQUACY_AGRODEALER_NETWORK = "E2.4 Adequacy of agrodealer network";
+export const C1_SATISFACTION_VARIETY_RELEASE_PROCESS = "C1. Satisfaction with variety release process";
+export const C2_SATISFACTION_SEED_REGULATIONS = "C2. Satisfaction with the enforcement of seed law/ regulations (opinion)";
+export const C4_ADEQUACY_GOVERNMENT_EFFORT_COUNTERFEIT_SEED = "C4. Adequacy of government efforts to eradicate counterfeit seed (opinion)";
--- ui/src/embeddable/reducers/StoreConstants.js
+++ ui/src/embeddable/reducers/StoreConstants.js
@@ -61,5 +61,6 @@ export const AVAILABILITY_BASIC_SEED = 'availabilityBasicSeed_map';
 export const ADEQUACY_SEED_INSPECTION_SERVICES = 'getRatingSeedInspectors_map';
 export const ADEQUACY_EXTENSION_SERVICES = "agriculturalExtensionServices_map";
 export const ADEQUACY_AGRODEALER_NETWORK = "agrodealerNetwork_map";
+export const SATISFACTION_VARIETY_RELEASE_PROCESS = "varietyReleaseProcess_map";
 
 export const WP_CHART = 'wp-chart';
