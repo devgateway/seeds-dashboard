2c02744b1c4b585333c0dfd9338a5fd7ad7f1757
ginchauspe <ginchauspe@gmail.com>
2022-10-21 11:02:26 -0300

    SEEDSDT-1055: Add translation.

--- ui/src/embeddable/backToTop/index.jsx
+++ ui/src/embeddable/backToTop/index.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Button, Container, Icon } from "semantic-ui-react";
 import './backToTop.scss';
+import {injectIntl} from "react-intl";
 
 
-const BackToTop = ({ 'data-behavior': behavior = 'smooth', 'data-label': label = 'Back To The Top', editing }) => {
+const BackToTop = ({ 'data-behavior': behavior = 'smooth', 'data-label': label = 'Back To The Top', editing, intl }) => {
     const isEditing = editing === 'true'
     const [visible, setVisible] = useState(false)
     const toggleVisible = () => {
@@ -26,6 +27,7 @@ const BackToTop = ({ 'data-behavior': behavior = 'smooth', 'data-label': label =
             window.removeEventListener('scroll', toggleVisible);
         }
     }, [])
+    const lnLabel = intl.formatMessage({id: label});
     if (isEditing) {
         return <Container fluid={true} className={'styles reports'}>
             <span>Place holder for back to bottom component with label: <b>{label}</b></span>
@@ -33,9 +35,9 @@ const BackToTop = ({ 'data-behavior': behavior = 'smooth', 'data-label': label =
     } else {
         return (<div className="sticky-bottom">
             <Button onClick={scrollToTop} style={{ display: visible ? 'inline' : 'none' }}> <Icon
-                name='chevron circle up' />{label}</Button>
+                name='chevron circle up' />{lnLabel}</Button>
         </div>);
     }
 }
 
-export default BackToTop;
+export default injectIntl(BackToTop);
